5712259ccf1d4b494e537074aa7d0188b2efd544
CoderUDream <173806056@qq.com>
2019-12-12 23:01:43 +0800

    1.增加线程池相关 2.增加内存异常情况

--- service-jvm-profiler/src/main/java/com/jiang/controller/JvmController.java
+++ service-jvm-profiler/src/main/java/com/jiang/controller/JvmController.java
@@ -1,25 +1,47 @@
 package com.jiang.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by liyujiang on 2019/12/10.
  */
+@Slf4j
 @RestController
 public class JvmController {
 
-    List<Object> list = new ArrayList();
+
+    ExecutorService threadPool = Executors.newFixedThreadPool(2);
+
+    class OOMObject {
+        // 1k
+        byte[] bytes = new byte[1024];
+    }
 
     @GetMapping("/heap")
     public String heap() {
+        threadPool.submit(() -> {
+            List<Object> list = new ArrayList();
+            try {
+                while (true) {
+                    list.add(new OOMObject());
+                }
+            } catch (Exception e) {
+                System.out.println("out of memory: " + e.toString());
+            }
+        });
 
-        while (true) {
-            list.add(new Object());
-        }
+        return "ok";
     }
 
+    @GetMapping("/info")
+    public String info() {
+        return "jvm-profiler";
+    }
 }
