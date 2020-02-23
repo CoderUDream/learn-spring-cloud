c1841710589af88cd7c3c38d97de5e75427e6dff
CoderUDream <173806056@qq.com>
2020-02-23 11:15:07 +0800

    1.新增hystrix使用案例

--- service-api/src/main/java/com/jiang/service/shading/jdbc/service/constants/Contants.java
+++
@@ -1,8 +0,0 @@
-package com.jiang.service.shading.jdbc.service.constants;
-
-/**
- * Created by liyujiang on 2020/1/22.
- */
-public class Contants {
-    public static final String MQ_GROUP = "DefaultCluster";
-}
